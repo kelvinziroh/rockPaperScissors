1f7cdcfacbbc4f983e6de565bcf21dabd4ade010
Kelvin Ziro <kelvin.ziroh@gmail.com>
2022-04-18 12:13:25 +0300

    feat: Compare the computer's random selection and user's input

--- js/script.js
+++ js/script.js
@@ -17,26 +17,18 @@ const computerSelection = computerPlay();
 // Get user input
 const playerSelection = prompt("Please select one between Rock, Paper and Scissors:").toLowerCase();
 
-
 // Compare user input with the random selection from the computer
-    /*
-     * 
-     * Both draw if...
-     * 
-     * both have the same selection...both draw
-     * 
-     * computer wins if...
-     * 
-     * computer selected rock and user selected scissors
-     * computer selected paper and user selected rock
-     * computer selected scissors and user selected paper
-     * 
-     * user wins if...
-     * 
-     * computer selected paper and user selected scissors
-     * computer selected scissors and user selected rock
-     * computer selected rock and user selected paper
-     * 
-     */
+
+if (computerSelection === playerSelection) {
+    // When it's a draw
+    console.log(`You draw! You both selected ${computerSelection}!`); 
+} else if (computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "paper" && playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper"){
+    // When the computer wins
+    console.log(`You lose, ${computerSelection} beats ${playerSelection}`); 
+} else {
+    // When the player wins
+    console.log(`You win, ${playerSelection} beats ${computerSelection}`); 
+}
+   
 // Output the winner
 
